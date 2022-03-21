72cbe5794fbcab610eedf6df24cab4f84802d4a9
mega12345mega <55769873+mega12345mega@users.noreply.github.com>
2022-03-20 22:57:31 -0700

    Add files via upload

---
+++ com/luneruniverse/packetlibv2/PacketListener.java
@@ -0,0 +1,21 @@
+package com.luneruniverse.packetlibv2;
+
+import java.io.IOException;
+
+import com.luneruniverse.packetlibv2.packets.Packet;
+
+/**
+ * Called when a packet is received
+ */
+@FunctionalInterface
+public interface PacketListener {
+	/**
+	 * Called when a packet is received
+	 * @param packet The packet that was received
+	 * @param connection What connection the packet is from
+	 * @param wait View the {@link WaitState} docs for information
+	 * @see ServerConnection
+	 * @see Client
+	 */
+	public void onPacket(Packet packet, Connection connection, WaitState wait) throws IOException;
+}
---
+++ com/luneruniverse/packetlibv2/packets/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Includes all the default packet types
+ * @see com.luneruniverse.packetlibv2.packets.Packet
+ * @see com.luneruniverse.packetlibv2.packets.PrimitivePacket
+ */
+package com.luneruniverse.packetlibv2.packets;
