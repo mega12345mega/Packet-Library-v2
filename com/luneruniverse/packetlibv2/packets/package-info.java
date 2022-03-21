fec8c1e97c4739ca7058f1708b3bb1ccc98358e2
mega12345mega <55769873+mega12345mega@users.noreply.github.com>
2022-03-20 23:06:59 -0700

    Delete com/luneruniverse/packetlibv2 directory

--- com/luneruniverse/packetlibv2/packets/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Includes all the default packet types
- * @see com.luneruniverse.packetlibv2.packets.Packet
- * @see com.luneruniverse.packetlibv2.packets.PrimitivePacket
- */
-package com.luneruniverse.packetlibv2.packets;
