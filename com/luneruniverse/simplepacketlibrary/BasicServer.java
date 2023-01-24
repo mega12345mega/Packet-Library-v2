92b3dd55294e615e47fec0d076b76c1c68d9978f
mega12345mega <55769873+mega12345mega@users.noreply.github.com>
2023-01-23 21:55:09 -0800

    Delete com/luneruniverse/simplepacketlibrary directory

--- com/luneruniverse/simplepacketlibrary/BasicServer.java
+++
@@ -1,8 +0,0 @@
-package com.luneruniverse.simplepacketlibrary;
-
-import java.io.Closeable;
-import java.io.IOException;
-
-interface BasicServer extends Closeable {
-	public BasicSocket accept() throws IOException, InterruptedException;
-}
