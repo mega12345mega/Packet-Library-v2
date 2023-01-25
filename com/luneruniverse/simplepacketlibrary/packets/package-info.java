61a49934aa42e695b6231d15e0b21d69dcabaf5e
mega12345mega <55769873+mega12345mega@users.noreply.github.com>
2023-01-24 23:25:22 -0800

    Delete com/luneruniverse/simplepacketlibrary directory

--- com/luneruniverse/simplepacketlibrary/packets/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Includes all the default packet types
- * @see com.luneruniverse.simplepacketlibrary.packets.Packet
- * @see com.luneruniverse.simplepacketlibrary.packets.PrimitivePacket
- */
-package com.luneruniverse.simplepacketlibrary.packets;
